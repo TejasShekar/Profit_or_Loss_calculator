57f87f22132597667b8053694c05c40de0441974
Tejas Shekar <68545229+TejasShekar@users.noreply.github.com>
2021-12-07 14:48:47 +0530

    big update
    Full project completed

--- main.js
+++ main.js
@@ -10,12 +10,16 @@ function calcProfitAndLoss(i, q, c) {
     var lossPercent = (loss / i) * 100;
     loss = loss.toFixed(2);
     lossPercent = lossPercent.toFixed(2);
+    output.style.color = 'red';
+    output.style.backgroundColor = 'white';
     output.innerText = `The loss is ${loss} and loss percentage is ${lossPercent}%`;
   } else if (i < c) {
     var profit = (c - i) * q;
     var profitPercent = (profit / i) * 100;
     profit = profit.toFixed(2);
     profitPercent = profitPercent.toFixed(2);
+    output.style.color = 'green';
+    output.style.backgroundColor = 'white';
     output.innerText = `The profit is ${profit} and profit percentage is ${profitPercent}%`;
   } else {
     output.innerText = `No pain no gain and no gain no pain`;
