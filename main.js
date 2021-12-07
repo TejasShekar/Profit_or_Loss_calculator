10be063c7b6e69d5083de5a3251792b6028edb03
Tejas Shekar <68545229+TejasShekar@users.noreply.github.com>
2021-12-07 14:13:55 +0530

    update main.js

---
+++ main.js
@@ -0,0 +1,7 @@
+const initialPrice = document.querySelector("#initial-p");
+const stocksQuantity = document.querySelector("#quantity");
+const currentPrice = document.querySelector("#current-p");
+const checkBtn = document.querySelector("#check-btn");
+const output = document.querySelector("#output");
+
+
